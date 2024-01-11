972262781c3daa4852ec70445ee11fd818a408d4
BuKwon Kim <dnu05043@naver.com>
2024-01-11 14:07:21 +0900

    hello, batch

--- src/main/java/com/example/springbootbatch/SpringbootBatchApplication.java
+++ src/main/java/com/example/springbootbatch/SpringbootBatchApplication.java
@@ -1,13 +1,13 @@
 package com.example.springbootbatch;
 
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@EnableBatchProcessing
 @SpringBootApplication
 public class SpringbootBatchApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(SpringbootBatchApplication.class, args);
     }
-
 }
