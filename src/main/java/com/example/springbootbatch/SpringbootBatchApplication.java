bb6c139fc82622ddcd84ef94d368b48b036cc674
BuKwon Kim <dnu05043@naver.com>
2024-01-15 10:11:45 +0900

    테스트 환경에서 배치 실행하는 방법

--- src/main/java/com/example/springbootbatch/SpringbootBatchApplication.java
+++ src/main/java/com/example/springbootbatch/SpringbootBatchApplication.java
@@ -1,10 +1,9 @@
 package com.example.springbootbatch;
 
-import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@EnableBatchProcessing
 @SpringBootApplication
 public class SpringbootBatchApplication {
     public static void main(String[] args) {
